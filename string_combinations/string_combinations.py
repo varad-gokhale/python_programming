9b1585d69abc2b61396f3346b48911eea9ccc20c
Varad Gokhale <varad.gokhale91@gmail.com>
2018-09-28 06:13:31 -0500

    string_combinations.py

--- string_combinations/string_combinations.py
+++ string_combinations/string_combinations.py
@@ -1,18 +1,20 @@
 import sys
 
 def string_combinations(s):
-	
+
+	str_set = set() 	
 	def helper(s, index):	
 		if index == len(s):
 			return
 		for i in range(index, len(s)):
-			print(s[index:i+1] + " ")
+			str_set.add(s[index:i+1])
 			helper(s, index+1)
 	
 	helper(s,0)
+	return str_set
 
 def main():
-	string_combinations(sys.argv[1])
+	print(string_combinations(sys.argv[1]))
 
 if __name__== "__main__":
 	main()
